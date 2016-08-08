4d840f7bc98d1862b230c916f4577a4fc770be74
LogicTechCorp <logictechcorp@gmail.com>
2016-08-07 21:06:53 -0700

    Forgot to remove

--- src/main/java/nex/api/NetherExAPI.java
+++ src/main/java/nex/api/NetherExAPI.java
@@ -2,22 +2,18 @@ package nex.api;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
-import net.minecraft.util.WeightedRandom;
 import net.minecraftforge.common.BiomeManager;
-import net.minecraftforge.common.DungeonHooks;
 import nex.api.biome.NetherBiome;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.List;
-import java.util.Random;
 
 public class NetherExAPI
 {
     public static final Logger LOGGER = LogManager.getLogger("NetherEx|API");
 
     private static List<BiomeManager.BiomeEntry> biomeEntries = Lists.newArrayList();
-    private static List<DungeonHooks.DungeonMob> dungeonMobs = Lists.newArrayList();
 
     public static void addBiome(NetherBiome biome, int weight)
     {
@@ -59,58 +55,8 @@ public class NetherExAPI
         });
     }
 
-    public static void addDungeonMob(String name, int weight)
-    {
-        if(name == null || name.equals(""))
-        {
-            LOGGER.warn("Unable to add Mob. It was null!");
-            return;
-        }
-        else if(weight <= 0)
-        {
-            LOGGER.warn(String.format("Unable to add %s to the Nether Dungeons. Its Weight must be greater than zero!", name));
-            return;
-        }
-
-        for(DungeonHooks.DungeonMob mob : dungeonMobs)
-        {
-            if(name.equals(mob.type))
-            {
-                mob.itemWeight += weight;
-                return;
-            }
-        }
-
-        dungeonMobs.add(new DungeonHooks.DungeonMob(weight, name));
-    }
-
-    public static void removeDungeonMob(String name)
-    {
-        if(name == null || name.equals(""))
-        {
-            LOGGER.warn("Unable to remove Mob. It was null!");
-            return;
-        }
-
-        dungeonMobs.stream().filter(mob -> name.equals(mob.type)).forEach(mob ->
-        {
-            dungeonMobs.remove(mob);
-            LOGGER.info(String.format("Removed %s from the Nether Dungeons.", name));
-        });
-    }
-
     public static ImmutableList<BiomeManager.BiomeEntry> getBiomeEntries()
     {
         return ImmutableList.copyOf(biomeEntries);
-    }
-
-    public static ImmutableList<DungeonHooks.DungeonMob> getDungeonMobs()
-    {
-        return ImmutableList.copyOf(dungeonMobs);
-    }
-
-    public static String getRandomDungeonMob(Random rand)
-    {
-        return WeightedRandom.getRandomItem(rand, dungeonMobs).type;
     }
 }
--- src/main/java/nex/registry/ModBiomes.java
+++ src/main/java/nex/registry/ModBiomes.java
@@ -1,7 +1,6 @@
 package nex.registry;
 
 import net.minecraft.world.biome.Biome;
-import nex.api.NetherExAPI;
 import nex.api.biome.NetherBiome;
 import nex.world.biome.*;
 
@@ -20,9 +19,6 @@ public class ModBiomes
         FUNGI_FOREST = new BiomeFungiForest(42, "fungi_forest", 20, new Biome.BiomeProperties("Fungi Forest"));
         BLAZING_INFERNO = new BiomeBlazingInferno(43, "blazing_inferno", 20, new Biome.BiomeProperties("Blazing Inferno"));
         FREEZING_BLIZZARD = new BiomeFreezingBlizzard(44, "freezing_blizzard", 10, new Biome.BiomeProperties("Freezing Blizzard"));
-
-        NetherExAPI.addDungeonMob("Blaze", 100);
-        NetherExAPI.addDungeonMob("LavaSlime", 100);
     }
 
     public static void register()
