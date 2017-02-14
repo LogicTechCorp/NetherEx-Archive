abbedff544fd8c53ae4887bf3704aeb513c6cb82
LogicTechCorp <logictechcorp@gmail.com>
2017-02-13 17:36:48 -0700

    Updated Nethermite model

--- src/main/java/nex/client/model/entity/ModelNethermite.java
+++ src/main/java/nex/client/model/entity/ModelNethermite.java
@@ -28,40 +28,48 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class ModelNethermite extends ModelBase
 {
     private ModelRenderer head;
-    private ModelRenderer upperBody;
-    private ModelRenderer lowerBody;
+    private ModelRenderer frontBody;
+    private ModelRenderer middleBody;
+    private ModelRenderer backBody;
     private ModelRenderer tail;
-    private ModelRenderer upperBodySpine;
-    private ModelRenderer lowerBodySpine;
-    private ModelRenderer tailSpineUpper;
-    private ModelRenderer tailSpineLower;
+    private ModelRenderer headSpine;
+    private ModelRenderer frontBodySpine;
+    private ModelRenderer middleBodySpine;
+    private ModelRenderer backBodySpine;
+    private ModelRenderer tailSpine;
 
     public ModelNethermite()
     {
         head = new ModelRenderer(this, 0, 0);
-        head.setRotationPoint(0.0F, 22.0F, -4.0F);
-        head.addBox(-1.5F, 0.0F, -0.5F, 3, 2, 1);
-        upperBody = new ModelRenderer(this, 0, 3);
-        upperBody.setRotationPoint(0.0F, 21.0F, -1.0F);
-        upperBody.addBox(-2.0F, 0.0F, -2.5F, 4, 3, 5);
-        lowerBody = new ModelRenderer(this, 0, 11);
-        lowerBody.setRotationPoint(0.0F, 22.0F, 4.0F);
-        lowerBody.addBox(-1.5F, 0.0F, -2.5F, 3, 2, 3);
-        tail = new ModelRenderer(this, 0, 16);
-        tail.setRotationPoint(0.0F, 23.0F, 5.0F);
+        head.setRotationPoint(0.0F, 21.0F, -4.0F);
+        head.addBox(-1.5F, 0.0F, -1.0F, 3, 3, 2);
+        frontBody = new ModelRenderer(this, 0, 5);
+        frontBody.setRotationPoint(0.0F, 20.0F, 0.0F);
+        frontBody.addBox(-2.0F, 0.0F, -3.0F, 4, 4, 6);
+        middleBody = new ModelRenderer(this, 0, 15);
+        middleBody.setRotationPoint(0.0F, 21.0F, 4.0F);
+        middleBody.addBox(-1.5F, 0.0F, -1.5F, 3, 3, 3);
+        backBody = new ModelRenderer(this, 0, 21);
+        backBody.setRotationPoint(0.0F, 22.0F, 6.0F);
+        backBody.addBox(-1.0F, 0.0F, -1.0F, 2, 2, 2);
+        tail = new ModelRenderer(this, 0, 25);
+        tail.setRotationPoint(0.0F, 23.0F, 8.0F);
         tail.addBox(-0.5F, 0.0F, -1.0F, 1, 1, 2);
-        upperBodySpine = new ModelRenderer(this, 18, 5);
-        upperBodySpine.setRotationPoint(0.0F, 20.0F, -1.0F);
-        upperBodySpine.addBox(0.0F, 0.0F, -2.5F, 0, 1, 5);
-        lowerBodySpine = new ModelRenderer(this, 12, 12);
-        lowerBodySpine.setRotationPoint(0.0F, 21.0F, 3.0F);
-        lowerBodySpine.addBox(0.0F, 0.0F, -1.5F, 0, 1, 3);
-        tailSpineUpper = new ModelRenderer(this, 6, 16);
-        tailSpineUpper.setRotationPoint(0.0F, 22.0F, 5.0F);
-        tailSpineUpper.addBox(0.0F, 0.0F, -1.0F, 0, 1, 2);
-        tailSpineLower = new ModelRenderer(this, 6, 19);
-        tailSpineLower.setRotationPoint(0.0F, 23.0F, 7.0F);
-        tailSpineLower.addBox(0.0F, 0.0F, -1.0F, 0, 1, 2);
+        headSpine = new ModelRenderer(this, 10, 2);
+        headSpine.setRotationPoint(0.0F, 20.0F, -4.0F);
+        headSpine.addBox(0.0F, 0.0F, -1.0F, 0, 1, 2);
+        frontBodySpine = new ModelRenderer(this, 20, 5);
+        frontBodySpine.setRotationPoint(0.0F, 19.0F, 0.0F);
+        frontBodySpine.addBox(0.0F, 0.0F, -3.0F, 0, 1, 6);
+        middleBodySpine = new ModelRenderer(this, 12, 17);
+        middleBodySpine.setRotationPoint(0.0F, 20.0F, 4.0F);
+        middleBodySpine.addBox(0.0F, 0.0F, -1.5F, 0, 1, 3);
+        backBodySpine = new ModelRenderer(this, 8, 22);
+        backBodySpine.setRotationPoint(0.0F, 21.0F, 6.0F);
+        backBodySpine.addBox(0.0F, 0.0F, -1.0F, 0, 1, 2);
+        tailSpine = new ModelRenderer(this, 6, 25);
+        tailSpine.setRotationPoint(0.0F, 22.0F, 8.0F);
+        tailSpine.addBox(0.0F, 0.0F, -1.0F, 0, 1, 2);
     }
 
     @Override
@@ -69,13 +77,15 @@ public class ModelNethermite extends ModelBase
     {
         setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, rotationYaw, rotationPitch, scale, entity);
         head.render(scale);
-        upperBody.render(scale);
-        lowerBody.render(scale);
+        frontBody.render(scale);
+        middleBody.render(scale);
+        backBody.render(scale);
         tail.render(scale);
-        upperBodySpine.render(scale);
-        lowerBodySpine.render(scale);
-        tailSpineUpper.render(scale);
-        tailSpineLower.render(scale);
+        headSpine.render(scale);
+        frontBodySpine.render(scale);
+        middleBodySpine.render(scale);
+        backBodySpine.render(scale);
+        tailSpine.render(scale);
     }
 
     @Override
@@ -83,19 +93,24 @@ public class ModelNethermite extends ModelBase
     {
         head.rotateAngleY = MathHelper.cos(ageInTicks * 0.9F + (float) 0 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.01F * (float) (1 + Math.abs(0 - 2));
         head.rotationPointX = MathHelper.sin(ageInTicks * 0.9F + (float) 0 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.1F * (float) Math.abs(0 - 2);
-        upperBody.rotateAngleY = MathHelper.cos(ageInTicks * 0.9F + (float) 1 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.01F * (float) (1 + Math.abs(1 - 2));
-        upperBody.rotationPointX = MathHelper.sin(ageInTicks * 0.9F + (float) 1 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.1F * (float) Math.abs(1 - 2);
-        lowerBody.rotateAngleY = MathHelper.cos(ageInTicks * 0.9F + (float) 2 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.01F * (float) (1 + Math.abs(2 - 2));
-        lowerBody.rotationPointX = MathHelper.sin(ageInTicks * 0.9F + (float) 2 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.1F * (float) Math.abs(2 - 2);
-        tail.rotateAngleY = MathHelper.cos(ageInTicks * 0.9F + (float) 3 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.01F * (float) (1 + Math.abs(3 - 2));
-        tail.rotationPointX = MathHelper.sin(ageInTicks * 0.9F + (float) 3 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.1F * (float) Math.abs(3 - 2);
-        upperBodySpine.rotateAngleY = upperBody.rotateAngleY;
-        upperBodySpine.rotationPointX = upperBody.rotationPointX;
-        lowerBodySpine.rotateAngleY = lowerBody.rotateAngleY;
-        lowerBodySpine.rotationPointX = lowerBody.rotationPointX;
-        tailSpineUpper.rotateAngleY = tail.rotateAngleY;
-        tailSpineUpper.rotationPointX = tail.rotationPointX;
-        tailSpineLower.rotateAngleY = tail.rotateAngleY;
-        tailSpineLower.rotationPointX = tail.rotationPointX;
+        frontBody.rotateAngleY = MathHelper.cos(ageInTicks * 0.9F + (float) 1 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.01F * (float) (1 + Math.abs(1 - 2));
+        frontBody.rotationPointX = MathHelper.sin(ageInTicks * 0.9F + (float) 1 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.1F * (float) Math.abs(1 - 2);
+        middleBody.rotateAngleY = MathHelper.cos(ageInTicks * 0.9F + (float) 2 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.01F * (float) (1 + Math.abs(2 - 2));
+        middleBody.rotationPointX = MathHelper.sin(ageInTicks * 0.9F + (float) 2 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.1F * (float) Math.abs(2 - 2);
+        backBody.rotateAngleY = MathHelper.cos(ageInTicks * 0.9F + (float) 3 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.01F * (float) (1 + Math.abs(3 - 2));
+        backBody.rotationPointX = MathHelper.sin(ageInTicks * 0.9F + (float) 3 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.1F * (float) Math.abs(3 - 2);
+        tail.rotateAngleY = MathHelper.cos(ageInTicks * 0.9F + (float) 3 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.01F * (float) (1 + Math.abs(4 - 2));
+        tail.rotationPointX = MathHelper.sin(ageInTicks * 0.9F + (float) 3 * 0.15F * (float) Math.PI) * (float) Math.PI * 0.1F * (float) Math.abs(4 - 2);
+
+        headSpine.rotateAngleY = head.rotateAngleY;
+        headSpine.rotationPointX = head.rotationPointX;
+        frontBodySpine.rotateAngleY = frontBody.rotateAngleY;
+        frontBodySpine.rotationPointX = frontBody.rotationPointX;
+        middleBodySpine.rotateAngleY = middleBody.rotateAngleY;
+        middleBodySpine.rotationPointX = middleBody.rotationPointX;
+        backBodySpine.rotateAngleY = backBody.rotateAngleY;
+        backBodySpine.rotationPointX = backBody.rotationPointX;
+        tailSpine.rotateAngleY = tail.rotateAngleY;
+        tailSpine.rotationPointX = tail.rotationPointX;
     }
 }
