e32815531d037692348c27b92a759c5af2397136
LogicTechCorp <logictechcorp@gmail.com>
2017-05-02 23:21:29 -0700

    Missed a few files

--- src/main/java/nex/block/BlockSoulGlass.java
+++ src/main/java/nex/block/BlockSoulGlass.java
@@ -20,7 +20,6 @@ package nex.block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
