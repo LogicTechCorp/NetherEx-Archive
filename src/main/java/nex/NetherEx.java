c5e18f864a8a1e22f379a4fcd163f1d0126b3ee6
LogicTechCorp <logictechcorp@gmail.com>
2018-07-25 03:38:02 -0700

    Updated required LEX version

--- src/main/java/nex/NetherEx.java
+++ src/main/java/nex/NetherEx.java
@@ -36,7 +36,7 @@ public class NetherEx implements IModData
     public static final String MOD_ID = "nex";
     public static final String NAME = "NetherEx";
     public static final String VERSION = "2.0.1";
-    public static final String DEPENDENCIES = "required-after:lex@[0.1.0,);";
+    public static final String DEPENDENCIES = "required-after:lex@[1.0.1,);";
     private static final String CLIENT_PROXY = "nex.proxy.CombinedClientProxy";
     private static final String SERVER_PROXY = "nex.proxy.DedicatedServerProxy";
 
