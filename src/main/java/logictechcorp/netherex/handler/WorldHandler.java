682f7c71037c03eae3ff12035587139241ff9a42
LogicTechCorp <logictechcorp@gmail.com>
2020-04-29 16:19:32 -0700

    Added global access transformer Fixed lingering clouds spawning the burning fire effect
    (cherry picked from commit d4460e1c2c02e2b8ca659d303c3a240506e2580c)

---
+++ src/main/java/logictechcorp/netherex/handler/WorldHandler.java
@@ -0,0 +1,53 @@
+/*
+ * NetherEx
+ * Copyright (c) 2016-2020 by LogicTechCorp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package logictechcorp.netherex.handler;
+
+import logictechcorp.netherex.NetherEx;
+import logictechcorp.netherex.potion.NetherExEffects;
+import net.minecraft.entity.AreaEffectCloudEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = NetherEx.MOD_ID)
+public class WorldHandler
+{
+    @SubscribeEvent
+    public static void onEntitySpawn(EntityJoinWorldEvent event)
+    {
+        World world = event.getWorld();
+        Entity entity = event.getEntity();
+
+        if(!world.isRemote())
+        {
+            if(entity instanceof AreaEffectCloudEntity)
+            {
+                AreaEffectCloudEntity areaEffectCloudEntity = (AreaEffectCloudEntity) entity;
+                areaEffectCloudEntity.effects.removeIf(effect -> effect.getPotion() == NetherExEffects.FIRE_BURNING.get());
+
+                if(areaEffectCloudEntity.effects.isEmpty())
+                {
+                    ((ServerWorld)world).removeEntity(entity);
+                }
+            }
+        }
+    }
+}
